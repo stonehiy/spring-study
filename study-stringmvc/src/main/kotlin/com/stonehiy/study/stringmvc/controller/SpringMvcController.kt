cbfca6446b4f8116497d473fb8dd34bf3d52883c
stonehiy <stonehiy@gmail.com>
2019-09-09 11:58:12 +0800

    add[study-springmvc]:springmvc-restful

--- study-stringmvc/src/main/kotlin/com/stonehiy/study/stringmvc/controller/SpringMvcController.kt
+++ study-stringmvc/src/main/kotlin/com/stonehiy/study/stringmvc/controller/SpringMvcController.kt
@@ -1,13 +1,14 @@
 package com.stonehiy.study.stringmvc.controller
 
+import com.stonehiy.study.stringmvc.entity.User
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
 
 
 @Controller
 class SpringMvcController {
-
 
 
     @RequestMapping(method = [RequestMethod.GET], value = ["/test"])
@@ -15,6 +16,17 @@ class SpringMvcController {
         //跳转到/views/test.jsp
         println("test .........")
         return "test"
+    }
+
+
+    @RequestMapping(method = [RequestMethod.GET], value = ["/restful"])
+    @ResponseBody
+    fun restful(): Any {
+        val user = User()
+        user.age = 1
+        user.username = "pack"
+        user.password = "123456"
+        return user
     }
 
 }
---
+++ study-stringmvc/src/main/kotlin/com/stonehiy/study/stringmvc/entity/User.kt
@@ -0,0 +1,8 @@
+package com.stonehiy.study.stringmvc.entity
+
+class User {
+    var username: String = "";
+    var password: String = "";
+    var name: String = "";
+    var age = 0;
+}
